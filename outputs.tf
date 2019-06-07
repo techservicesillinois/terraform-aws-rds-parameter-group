472bd98d3813f476600a13852ee1c39e9142b40c
Jeffrey Dimpsey <dimpsey@illinois.edu>
2019-06-07 11:23:07 -0500

    initial dev commit of module 2

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+## outputs
+output "id" {
+ value = "${aws_db_parameter_group.default.id}"
+}
+
+output "name" {
+ value = "${aws_db_parameter_group.default.name}"
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+ description = "(required) The name of the DB parameter group. If omitted, Terraform will assign a random, unique name."
+}
+
+variable "family" {
+ description = "(Required) The family of the DB parameter group."
+}
+
+variable "parameters" {
+  description = "A list of DB parameters (map) to apply"
+  default     = []
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources"
+  default     = {}
+}
